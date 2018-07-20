4046fd0efc7e83e420af5e11f36e3e446025f16f
clei1 <callmecon328@gmail.com>
2018-07-20 21:44:22 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,26 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  return {"Sydney": "2000", "Athens": "2004", "Beijing": "2008", "London": "2012"}
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  olym = create_olympics_hash
+  olym[:"Atlanta"] = "1996"
+  olym
 end
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  add_a_key_value_pair.each do |city, year|
+    puts "The #{city} summer olympics took place in #{year}."
+  end
 end
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  add_a_key_value_pair.keys
 end
